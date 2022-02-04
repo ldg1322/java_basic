3df6d30ad1ce234edd1f05c607b42844b1971869
ldg1322 <ldg1322@naver.com>
2022-02-04 21:38:44 +0900

    Arrays, Math

--- 20220203/src/stringbuilder1/Builder05P408.java
+++ 20220203/src/stringbuilder1/Builder05P408.java
@@ -6,8 +6,20 @@ public class Builder05P408 {
 		// 다음 문자열의 문자를 0번부터 시작해서 3번째마다 삭제해주세요.
 		// 힌트 : 반복문을 쓰세요
 		// 문자열은 "asdfasdfasdf"입니다.
-		StringBuilder a = new StringBuilder("qwerasdfzxcv");
-
+		StringBuilder a = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+		System.out.println(a);
+		int checkNum = a.length() % 3;
+		
+		System.out.println(checkNum);
+		
+		int loopNum = a.length() - checkNum;
+		
+		for(int i = loopNum; i > -1; i--) {
+			if(i % 3 == 0) {
+				a.deleteCharAt(i);
+			}
+		}
+		System.out.println(a);
 	}
 
 }
